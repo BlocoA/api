7a23a8021ea8c8e0e4ce0a03790d15758a760758
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-05-27 22:58:35 -0300

    Add Condominium CRUD
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ app/controllers/v1/condominiums_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module V1
+  class CondominiumsController < ApplicationController
+    before_action :set_condominium, only: %i[show update destroy]
+
+    def show
+      render json: @condominium
+    end
+
+    def create
+      condominium = Condominium.new(condominium_params)
+
+      if condominium.save
+        render json: condominium
+      else
+        errors = condominium.errors.full_messages.join(', ')
+        render json: { errors: errors }, status: :bad_request
+      end
+    end
+
+    def update
+      if @condominium.update(condominium_params)
+        render json: @condominium
+      else
+        errors = @condominium.errors.full_messages.join(', ')
+        render json: { errors: errors }, status: :bad_request
+      end
+    end
+
+    def destroy
+      if @condominium.destroy
+        render json: {}, status: :no_content
+      else
+        errors = @condominium.errors.full_messages.join(', ')
+        render json: { errors: errors }, status: :bad_request
+      end
+    end
+
+    private
+
+    def set_condominium
+      @condominium = Condominium.find(params[:id])
+    end
+
+    def condominium_params
+      params.require(:condominium).permit(:name, :units_quantity, :has_leisure_area)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: proc { [200, {}, ['ok']] }
+
+  namespace :v1 do
+    resources :condominiums, only: %i[show create update destroy]
+  end
 end
