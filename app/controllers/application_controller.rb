807f6732d14816eeb7870196d64cb699021d6637
Anderson Fernandes <fernandesanderson14@gmail.com>
2022-05-28 21:01:18 -0300

    Move concern from authenticable to authorizable
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  include Authenticable
+  include Authorizable
 
   rescue_from ActiveRecord::RecordNotFound do |error|
     render json: { errors: error.message }, status: :not_found
